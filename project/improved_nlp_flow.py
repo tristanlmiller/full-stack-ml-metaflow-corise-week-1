1447b672c67109e1250e4ae632a871e20136bd9a
Tristan <tristanlmiller@gmail.com>
2023-10-20 20:43:04 +0000

    complete most of debugging

--- project/improved_nlp_flow.py
+++ project/improved_nlp_flow.py
@@ -45,6 +45,7 @@ class ImprovedNLPFlow(FlowSpec):
 
     @step
     def preprocess(self):
+        import string
         from sklearn.feature_extraction.text import CountVectorizer
         import nltk
         import numpy as np
@@ -66,7 +67,7 @@ class ImprovedNLPFlow(FlowSpec):
 
         #vectorize
         vectorizer = CountVectorizer(analyzer = "word",min_df=0.05,max_features=25)
-        bag_of_words = vectorizer.fit_transform(df['review_text'].head()).toarray()
+        bag_of_words = vectorizer.fit_transform(processed_reviews).toarray()
         
         #drop ignore list
         ignore_list = ['dress','love','like','wear','great','im','would','really','ordered','perfect','one','flattering','well','nice']
@@ -82,8 +83,8 @@ class ImprovedNLPFlow(FlowSpec):
 
         # split the data 80/20, or by using the flow's split-sz CLI argument
         #add a random_state here for reproducibility
-        self.x_train, self.y_train, self.x_val, self.y_val, self.val = \
-            train_test_split(bag_of_words, self.df['label'], test_size=self.split_size,random_state=2023)
+        self.x_train, self.x_val, self.y_train, self.y_val, _, self.review_val = \
+            train_test_split(bag_of_words, self.df['label'],self.df['review_text'], test_size=self.split_size,random_state=2023)
         print(f"num of rows in train set: {self.x_train.shape[0]}")
         print(f"num of rows in validation set: {self.x_val.shape[0]}")
 
@@ -96,12 +97,12 @@ class ImprovedNLPFlow(FlowSpec):
         from sklearn.metrics import accuracy_score, roc_auc_score
 
         model = LogisticRegression()
-        model = fit(self.x_train, self.y_train)
-        
-        self.pred_val = model.predict(x_val)
-        self.prob_val = model.predict_proba(x_val)[:,0]
-        self.acc = accuracy_score(self.valdf["label"], self.pred_val)
-        self.rocauc = roc_auc_score(self.valdf["label"], self.prob_val)
+        model.fit(self.x_train, self.y_train)
+        self.model = model
+        self.pred_val = model.predict(self.x_val)
+        self.prob_val = model.predict_proba(self.x_val)[:,0]
+        self.acc = accuracy_score(self.y_val, self.pred_val)
+        self.rocauc = roc_auc_score(self.y_val, self.prob_val)
 
         self.next(self.end)
 
@@ -113,21 +114,21 @@ class ImprovedNLPFlow(FlowSpec):
         import numpy as np
         import pandas as pd
         msg = "Model Accuracy: {}\Model AUC: {}"
-        print(msg.format(round(self.base_acc, 3), round(self.base_rocauc, 3)))
+        print(msg.format(round(self.acc, 3), round(self.rocauc, 3)))
 
         current.card.append(Markdown("# Womens Clothing Review Results"))
         current.card.append(Markdown("## Overall Accuracy"))
-        current.card.append(Artifact(self.base_acc))
+        current.card.append(Artifact(self.acc))
 
         current.card.append(Markdown("## Thematic words and their coefficients"))
         coefs = pd.DataFrame({'Word':self.vocab,'Coefficient':self.model.coef_[0]})
-        coefs = coef.sort_values('Coefficient',key=abs,ascending=False)
+        coefs = coefs.sort_values('Coefficient',key=abs,ascending=False)
         current.card.append(Table.from_dataframe(coefs))
 
         current.card.append(Markdown("## Examples of False Positives"))
         false_pos = (self.y_val==0) & (self.pred_val==1)
         if np.sum(false_pos) > 0:
-            fp_reviews = self.valdf.loc[false_pos,'review_text'].sample(2)
+            fp_reviews = self.review_val.loc[false_pos].sample(2)
             current.card.append(Markdown('\n\n'.join(fp_reviews)))
         else:
             current.card.append(Markdown('none'))
@@ -137,7 +138,7 @@ class ImprovedNLPFlow(FlowSpec):
         current.card.append(Markdown("## Examples of False Negatives"))
         false_neg = (self.y_val==1) & (self.pred_val==0)
         if np.sum(false_neg) > 0:
-            fn_reviews = self.valdf.loc[false_neg,'review_text'].sample(2)
+            fn_reviews = self.review_val.loc[false_neg].sample(2)
             current.card.append(Markdown('\n\n'.join(fn_reviews)))
         else:
             current.card.append(Markdown('none'))
